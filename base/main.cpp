f43e1705de3ceb5af4c87fd0299e85ba65a3fcae
gustafsson1david <gustafsson1david@gmail.com>
2021-12-23 18:35:26 +0100

    Add base.

---
+++ base/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+const string INPUT_FILE_NAME = "input.txt";
+
+void readData() 
+{
+    ifstream inFile(INPUT_FILE_NAME.c_str());
+}
+
+void runPartOne()
+{
+    cout << "Part 1: " << endl;
+}
+
+void runPartTwo()
+{
+    cout << "Part 2: " << endl;
+}
+
+
+int main()
+{
+    readData();
+    runPartOne();
+    runPartTwo();
+    return 0;
+}
